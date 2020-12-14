5fbef24581caf0d8c07c61bf2df1a95af4c6c077
slinny <siranli411@gmail.com>
2020-12-14 18:46:28 -0500

    added onclicklistener to recycler viewholder

--- app/src/main/java/com/example/android/trafficsign/recyclerview/SignViewHolder.java
+++ app/src/main/java/com/example/android/trafficsign/recyclerview/SignViewHolder.java
@@ -1,21 +1,27 @@
 package com.example.android.trafficsign.recyclerview;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android.trafficsign.InfoActivity;
 import com.example.android.trafficsign.R;
 import com.example.android.trafficsign.model.SignModel;
 
-public class SignViewHolder extends RecyclerView.ViewHolder {
+public class SignViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private TextView textView;
+    private Context context;
 
     public SignViewHolder(View view) {
         super(view);
         textView = view.findViewById(R.id.sign_name_textview);
+        context = view.getContext();
+        view.setOnClickListener(this);
     }
 
     public void onBind(final SignModel signModel) {
@@ -26,5 +32,12 @@ public class SignViewHolder extends RecyclerView.ViewHolder {
             textView.setText(signModel.getName());
             textView.setTextColor(Color.GRAY);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(context, InfoActivity.class);
+        intent.putExtra("message", "Test Message");
+        context.startActivity(intent);
     }
 }
